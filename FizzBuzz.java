4fa9ff4e46d290880f827bae8c0706f5d007869a
pujasingh1 <52859941+pujasingh1@users.noreply.github.com>
2020-10-28 11:25:43 -0700

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,12 +5,12 @@ public class FizzBuzz
         for(int i = 1; i<=100; i++)
         {
             if(i % 3 == 0)
-            System.out.print("Fizz");
+            System.out.println("Fizz");
             if(i%5==0)
-            System.out.print("Buzz");
-            else if(i%3 != 0)
-            System.out.print(i);
-            System.out.println();
+            System.out.println("Buzz");
+            else if(i%15== 0)
+            System.out.println("FizzBuzz");
+            System.out.println(i+" ");
         }
     }
 }
